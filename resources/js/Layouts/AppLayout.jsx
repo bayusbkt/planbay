8bed3fba020715f08111763a5739f442f76550e7
bayy <bayusubekti00@gmail.com>
2024-12-29 11:36:48 +0700

    Add: Sidebar Component and refactoring Login Page

--- resources/js/Layouts/AppLayout.jsx
+++ resources/js/Layouts/AppLayout.jsx
@@ -2,7 +2,9 @@ import { Avatar, AvatarFallback } from '@/Components/ui/avatar';
 import { Dialog, Transition } from '@headlessui/react';
 import { Head, Link } from '@inertiajs/react';
 import { Fragment, useState } from 'react';
-import { PiHouse, PiLockKeyOpen, PiPlus, PiSidebar, PiSquaresFour, PiUser, PiX } from 'react-icons/pi';
+import { PiSidebar, PiX } from 'react-icons/pi';
+import Sidebar from './Partials/Sidebar';
+import SidebarResponsive from './Partials/SidebarResponsive';
 
 export default function AppLayout({ children, title }) {
     const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -56,7 +58,9 @@ export default function AppLayout({ children, title }) {
                                             </button>
                                         </div>
                                     </Transition.Child>
+
                                     {/* Sidebar Responsive */}
+                                    <SidebarResponsive />
                                 </Dialog.Panel>
                             </Transition.Child>
                         </div>
@@ -75,83 +79,7 @@ export default function AppLayout({ children, title }) {
                         </div>
 
                         {/* Sidebar */}
-                        <nav className="flex flex-1 flex-col">
-                            <ul role="list" className="flex flex-1 flex-col gap-y-7">
-                                <li>
-                                    <ul role="list" className="-mx-2 space-y-2">
-                                        {/* menu */}
-                                        <li>
-                                            <Link
-                                                href="#"
-                                                className="group flex gap-x-3 rounded-md p-3 text-sm font-semibold leading-relaxed tracking-tighter text-foreground hover:bg-gray-100"
-                                            >
-                                                <PiHouse className="h-6 w-6 shrink-0 text-foreground" /> Dashboard
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link
-                                                href="#"
-                                                className="group flex gap-x-3 rounded-md p-3 text-sm font-semibold leading-relaxed tracking-tighter text-foreground hover:bg-gray-100"
-                                            >
-                                                <PiUser className="h-6 w-6 shrink-0 text-foreground" /> People
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link
-                                                href="#"
-                                                className="group flex gap-x-3 rounded-md p-3 text-sm font-semibold leading-relaxed tracking-tighter text-foreground hover:bg-gray-100"
-                                            >
-                                                <PiSquaresFour className="h-6 w-6 shrink-0 text-foreground" /> My Task
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link
-                                                href="#"
-                                                className="group flex gap-x-3 rounded-md p-3 text-sm font-semibold leading-relaxed tracking-tighter text-foreground hover:bg-gray-100"
-                                            >
-                                                <PiLockKeyOpen className="h-6 w-6 shrink-0 text-foreground" /> Logout
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    {/* workspaces */}
-                                    <div className="flex items-center justify-between">
-                                        <div className="text-xs font-semibold uppercase leading-relaxed text-foreground">
-                                            Workspace
-                                        </div>
-                                        <Link>
-                                            <PiPlus className="h-4 w-4 text-foreground hover:text-red-500" />
-                                        </Link>
-                                    </div>
-                                    <ul className="-mx-2 mt-2 space-y-2">
-                                        <li>
-                                            <Link
-                                                href="#"
-                                                className="group flex w-full gap-x-3 rounded-md p-3 text-sm font-semibold leading-relaxed text-foreground hover:bg-gray-100"
-                                            >
-                                                <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-foreground bg-white text-[0.625rem] font-medium text-foreground">
-                                                    B
-                                                </span>
-                                                <span className="truncate">Backend Developer</span>
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li className="-mx-6 mt-auto">
-                                    {/* profile */}
-                                    <Link
-                                        href="#"
-                                        className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-relaxed text-foreground hover:bg-gray-100"
-                                    >
-                                        <Avatar>
-                                            <AvatarFallback>X</AvatarFallback>
-                                        </Avatar>
-                                        <span>Bayu Subekti</span>
-                                    </Link>
-                                </li>
-                            </ul>
-                        </nav>
+                        <Sidebar />
                     </div>
                 </div>
 
