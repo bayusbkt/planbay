5877a9d584f5bf73c21c16b1c1f4a271defbbd6e
bayy <bayusubekti00@gmail.com>
2024-12-31 19:10:05 +0700

    Feat: Edit Card

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -21,27 +21,30 @@ class CardController extends Controller
                 'title' => 'Create Card',
                 'subtitle' => 'Fill out this form to add a new card',
                 'method' => 'POST',
-                'action' => route('cards.store', $workspace)
+                'action' => route('cards.store', $workspace),
             ],
             'status' => request()->status ?? 'To Do',
             'statuses' => CardStatus::options(),
             'priority' => request()->priority ?? CardPriority::UNKNOWN->value,
             'priorities' => CardPriority::options(),
-            'workspace' => fn() => $workspace->only('slug')
+            'workspace' => fn() => $workspace->only('slug'),
         ]);
     }
 
     public function store(Workspace $workspace, CardRequest $request): RedirectResponse
     {
-        $card = $request->user()->cards()->create([
-            'workspace_id' => $workspace->id,
-            'title' => $request->title,
-            'description' => $request->description,
-            'deadline' => $request->deadline,
-            'status' => $status = $request->status,
-            'order' => $this->ordering($workspace, $status),
-            'priority' => $request->priority
-        ]);
+        $card = $request
+            ->user()
+            ->cards()
+            ->create([
+                'workspace_id' => $workspace->id,
+                'title' => $request->title,
+                'description' => $request->description,
+                'deadline' => $request->deadline,
+                'status' => ($status = $request->status),
+                'order' => $this->ordering($workspace, $status),
+                'priority' => $request->priority,
+            ]);
 
         flashMessage('Card information saved successfully');
 
@@ -54,17 +57,73 @@ class CardController extends Controller
             'card' => new CardSingleResource($card->load(['members', 'user', 'tasks', 'attachments'])),
             'page_settings' => [
                 'title' => 'Detail Card',
-                'subtitle' => 'You can see card information'
-            ]
+                'subtitle' => 'You can see card information',
+            ],
         ]);
+    }
+
+    public function edit(Workspace $workspace, Card $card): Response
+    {
+        return inertia('Cards/Edit', [
+            'card' => new CardSingleResource($card->load(['members', 'user', 'tasks', 'attachments'])),
+            'page_settings' => [
+                'title' => 'Edit Card',
+                'subtitle' => 'Fill out this form to edit card',
+                'method' => 'PUT',
+                'action' => route('cards.update', [$workspace, $card]),
+            ],
+            'statuses' => CardStatus::options(),
+            'priorities' => CardPriority::options(),
+            'workspace' => fn() => $workspace->only('slug'),
+        ]);
+    }
+
+    public function update(Workspace $workspace, Card $card, CardRequest $request): RedirectResponse
+    {
+        $last_status = $card->status->value;
+
+        $card->update([
+            'title' => $request->title,
+            'description' => $request->description,
+            'deadline' => $request->deadline,
+            'status' => ($status = $request->status),
+            'priority' => $request->priority,
+            'order' => $this->ordering($workspace, $status),
+        ]);
+
+        $this->adjustOrdering($workspace, $last_status);
+
+        flashMessage('Successfully updated card information');
+
+        return back();
     }
 
     public function ordering(Workspace $workspace, string $status): int
     {
-        $last_card = Card::query()->where('workspace_id', $workspace->id)->where('status', $status)->orderByDesc('order')->first();
+        $last_card = Card::query()
+            ->where('workspace_id', $workspace->id)
+            ->where('status', $status)
+            ->orderByDesc('order')
+            ->first();
 
-        if (!$last_card) return 1;
+        if (!$last_card) {
+            return 1;
+        }
 
         return $last_card->order + 1;
+    }
+
+    public function adjustOrdering(Workspace $workspace, string $status)
+    {
+        $order = 1;
+        return Card::where('workspace_id', $workspace->id)
+            ->where('status', $status)
+            ->orderBy('order')
+            ->get()
+            ->each(function ($card) use (&$order) {
+                $card->order = $order;
+                $card->save();
+                $order++;
+            });
     }
 }
---
+++ resources/js/Pages/Cards/Edit.jsx
@@ -0,0 +1,18 @@
+import HeaderForm from '@/Components/HeaderForm';
+import AppLayout from '@/Layouts/AppLayout';
+import UpdateCard from './UpdateCard';
+
+export default function Edit({ card, page_settings, statuses, priorities, workspace }) {
+    return (
+        <>
+            <div className="space-y-10 divide-y divide-dashed divide-gray-900/10">
+                <div className="grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-3">
+                    <HeaderForm title={page_settings.title} subtitle={page_settings.subtitle} />
+                    <UpdateCard page_settings={page_settings} statuses={statuses} priorities={priorities} card={card} />
+                </div>
+            </div>
+        </>
+    );
+}
+
+Edit.layout = (page) => <AppLayout children={page} title={page.props.page_settings.title} />;
