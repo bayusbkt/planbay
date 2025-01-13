739772d97685550ecd34e0a4db9a3aeaf99b4339
bayy <bayusubekti00@gmail.com>
2025-01-13 11:18:00 +0700

    Feat: Dashboard statistic

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\CardStatus;
+use App\Models\Card;
+use App\Models\Member;
+use App\Models\User;
+use App\Models\Workspace;
 use Illuminate\Http\Request;
 use Inertia\Response;
 
 class DashboardController extends Controller
 {
-    public function index(): Response {
-        return Inertia('Dashboard');
+    public function index(): Response
+    {
+        return Inertia('Dashboard', [
+            'page_settings' => [
+                'title' => 'Dashboard',
+                'subtitle' => 'You can see a summary of the information here'
+            ],
+            'count' => [
+                'users' => fn() => User::count(),
+                'workspaces' => fn() => Member::query()
+                    ->where('members.user_id', request()->user()->id)
+                    ->whereHasMorph('memberable', Workspace::class)
+                    ->count(),
+                'tasks' => fn() => Member::query()
+                    ->where('members.user_id', request()->user()->id)
+                    ->whereHasMorph('memberable', Card::class)
+                    ->count(),
+                'dones' => fn() => Member::query()
+                    ->where('members.user_id', request()->user()->id)
+                    ->whereHasMorph('memberable', Card::class, fn($query) => $query->where('status', CardStatus::DONE->value))
+                    ->count(),
+            ]
+        ]);
     }
 }
