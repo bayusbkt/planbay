8360e8ba518c7be147a0f9d12607f02eab8a187d
bayy <bayusubekti00@gmail.com>
2025-01-13 17:01:33 +0700

    last commit

--- resources/js/Pages/Testing.jsx
+++
@@ -1,7 +0,0 @@
-import AppLayout from '@/Layouts/AppLayout';
-
-export default function Testing() {
-    return <div>Testing...</div>;
-}
-
-Testing.layout = (page) => <AppLayout children={page} title="Testing" />;
